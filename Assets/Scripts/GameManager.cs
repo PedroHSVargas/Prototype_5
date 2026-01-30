1b5ac3eb61e3fc0a10f4ec04a6bebc72ca7e4a2d
Pedro Vargas <103851885+PedroHSVargas@users.noreply.github.com>
2026-01-29 22:47:45 -0300

    Coroutine to spawn objects

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+
+{
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
